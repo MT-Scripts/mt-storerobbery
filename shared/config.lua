97dd80e58e7dad331569eb464c9f79250199daf0
Butter Chilly <88847062+Butterchilly@users.noreply.github.com>
2024-05-28 11:00:24 +0530

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,8 +1,29 @@
 Config = {}
 
-Config.Notify = 'ox' -- qb or ox 
+Config.Debug = false
+
+
+Config.Notify = 'ox' -- \\ qb or ox 
+Config.Target = 'qb' -- \\ qb or ox
 
 Config.Dispatch = 'qb' -- qb or ps-dispatch or cd_dispatch
 Config.Minigame = 'ps-ui' -- ps-ui or ox_lib or qb-lock
 
 Config.requiredCopsCount  = 0
+
+
+Config.Registers_1 = {
+    [1] = { ["coords"] = vector3(-49.9, -1753.01, 29.42)}
+}
+
+Config.Shelf_1 = {
+    [1] = { ["coords"] = vector3(-49.9, -1753.01, 29.42)}
+}
+
+Config.Shelf_2 = {
+    [1] = { ["coords"] = vector3(-49.9, -1753.01, 29.42)}
+}
+
+Config.Safe = {
+    [1] = { ["coords"] = vector3(-49.9, -1753.01, 29.42)}
+}
